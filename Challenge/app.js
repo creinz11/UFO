1dcfe809b0a46c75e1be223414c55f10eb588a90
creinz11 <80016496+creinz11@users.noreply.github.com>
2021-05-30 11:17:53 -0500

    Add files via upload

--- Challenge/app.js
+++ Challenge/app.js
@@ -30,12 +30,12 @@ var filters = {};
 function updateFilters() {
 
   // 4a. Save the element that was changed as a variable.
-  let changedElement = d3.select(this);
+  let elementChanged = d3.select(this);
   // 4b. Save the value that was changed as a variable.
-  let elementValue = changedElement.property("value");
+  let elementValue = elementChanged.property("value");
   console.log(elementValue);
   // 4c. Save the id of the filter that was changed as a variable.
-  let filterId = changedElement.attr("id");
+  let filterId = elementChanged.attr("id");
 
   // 5. If a filter value was entered then add that filterId and value
   // to the filters list. Otherwise, clear that filter from the filters object.
